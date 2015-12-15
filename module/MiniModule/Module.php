20ff620aeea9b4d617049f060ad71948bcaa3132
Antoine <antoinebabu85@gmail.com>
2015-12-15 09:23:13 +0100

    Bootstrap partout avec EVENT_RENDER

--- module/MiniModule/Module.php
+++ module/MiniModule/Module.php
@@ -5,13 +5,13 @@ use Zend\ModuleManager\Feature\BootstrapListenerInterface;
 use Zend\EventManager\EventInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\Mvc\MvcEvent;
+use Zend\View\Model\ViewModel;
 class Module implements ConfigProviderInterface, BootstrapListenerInterface, AutoloaderProviderInterface
 {
     public function getConfig()
     {
         return include __DIR__ . "/config/module.config.php";
     }
-    
     public function onBootstrap(EventInterface $e)
     {
         // Récupération des erreurs en ajoutant un callback qui affiche l'erreur coté serveur
@@ -19,12 +19,18 @@ class Module implements ConfigProviderInterface, BootstrapListenerInterface, Aut
         $event = $application->getEventManager();
         $event->attach(MvcEvent::EVENT_DISPATCH_ERROR, function (MvcEvent $e) {
             error_log('DISPATCH_ERROR : ' . $e->getError());
-            //error_log($e->getControllerClass() . ' ' . $e->getController());
-            //error_log("Exception :".$e->getParam("router-no-match")->getMessage());
+            error_log($e->getControllerClass() . ' ' . $e->getController());
         });
         $event->attach(MvcEvent::EVENT_RENDER_ERROR, function (MvcEvent $e) {
             error_log('RENDER_ERROR : ' . $e->getError());
-            error_log("Exception :".$e->getParam("exception")->getMessage());
+        });
+        $event->attach(MvcEvent::EVENT_RENDER, function (MvcEvent $e) {
+            $services = $e->getApplication()->getServiceManager();
+            $form = $services->get('MiniModule\Form\Authentification');
+            $view = $e->getViewModel(); // c'est le viewModel qui contient le layout (top viewModel)
+            $formViewManager = new ViewModel( array( 'form' => $form ) );
+            $formViewManager->setTemplate( 'layout/form-auth');
+            $view->addChild( $formViewManager, 'formulaireAuth');
         });
     }
     public function getAutoloaderConfig()
